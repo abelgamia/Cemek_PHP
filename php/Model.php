f001a62a6c2de3fbd43a4b9dbb1df4d085cb3f9e
Edikowy <edikowy@vp.pl>
2019-07-27 01:01:55 +0200

    pocz model i user
    Signed-off-by: Edikowy <edikowy@vp.pl>

--- php/Model.php
+++ php/Model.php
@@ -1,6 +1,9 @@
 <?php
 namespace php;
 
+use PDO;
+use PDOException;
+
 /**
  *
  * @author Edi
@@ -8,5 +11,29 @@ namespace php;
  */
 abstract class Model
 {
+    protected $conn;                       // Uchwyt połączenia z bazą danych
+    protected $config = array(             // Tablica konfiguracji połączenia
+        'host'     => 'localhost',
+        'user'     => 'root',
+        'password' => '',
+        'database' => 'cemek'
+        // 'host'     => 'sql.5v.pl',
+        // 'user'     => 'db-user45519',
+        // 'password' => 'becederia1',
+        // 'database' => 'db-user45519'
+    );
+    /**
+     * Konstruktor klasy Baza
+     */
+    function __construct()
+    {
+        try {
+            $this->conn = new PDO('mysql:host='.$this->config['host'].';dbname='.$this->config['database'].';charset=utf8',
+                $this->config['user'], $this->config['password']);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            exit();
+        }
+    }
 }
 
--- php/model/User.php
+++ php/model/User.php
@@ -1,12 +1,176 @@
 <?php
 namespace php\model;
 
+use PDO;
+use php\Model;
+use function php\model\User\setHaslo;
+use function php\model\User\setLogin;
+
 /**
  *
  * @author Edi
  *        
  */
-class User
+class User extends Model
 {
+    private $id;
+    private $login;
+    private $haslo;
+    private $email;
+    private $access;
+    private $loged;
+    
+    function __construct() {
+        parent::__construct();
+        if (!empty($_SESSION[login]) && !empty($_SESSION[haslo])) {
+            addslashes($_SESSION['login']);
+            setLogin($_SESSION['login']);
+            addslashes($_SESSION['haslo']);
+            setHaslo($_SESSION['haslo']);
+            $sql = "SELECT * FROM uzytkownicy WHERE user = 'getLogin()'";
+            $query = $this->conn->prepare($sql);
+            $query->execute();
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+            if (!empty($result)){
+                if (password_verify(getHaslo(), $result['pass'])) {
+                    setId($result['id']);
+                    $_SESSION['id'] = getId();
+                    setEmail($result['email']);
+                    $_SESSION['email'] = getEmail();
+                    setAccess($result['access']);
+                    $_SESSION['access'] = getAccess();
+                    setLoged(TRUE);
+                    $_SESSION['loged'] = TRUE;
+                }
+            }
+        }
+    }
+    
+    private function logIn($login, $haslo)
+    {
+        
+    }
+    
+    private function logOut()
+    {
+        
+    }
+    
+    private function register($new_login ,$new_email ,$new_haslo ,$new_haslo2 ,$regulamin)
+    {
+        
+    }
+    
+    private function chengeEmail($new_email, $haslo, $potwierdz)
+    {
+        
+    }
+    
+    private function chengeHaslo($old_haslo, $new_haslo ,$new_haslo2 ,$potwierdz)
+    {
+        
+    }
+    
+    private function delUser()
+    {
+        
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHaslo()
+    {
+        return $this->haslo;
+    }
+
+    /**
+     * @param mixed $haslo
+     */
+    public function setHaslo($haslo)
+    {
+        $this->haslo = $haslo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccess()
+    {
+        return $this->access;
+    }
+
+    /**
+     * @param mixed $access
+     */
+    public function setAccess($access)
+    {
+        $this->access = $access;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getLoged()
+    {
+        return $this->loged;
+    }
+
+    /**
+     * @param boolean $loged
+     */
+    public function setLoged($loged)
+    {
+        $this->loged = $loged;
+    }
+    
 }
 
