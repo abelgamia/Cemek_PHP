9a1430311fdae62778580e33062d776da524eb07
Edikowy <39551059+Edikowy@users.noreply.github.com>
2019-12-25 10:58:25 +0100

    Delete inkludy.php

--- Cemek_PHP/src/inkludy.php
+++
@@ -1,9 +0,0 @@
-<?php
-// na wszelki pożarny...
-include 'src/Config.php';
-include 'src/Control.php';
-include 'src/Engine.php';
-include 'src/Model.php';
-include 'src/User.php';
-include 'src/Util.php';
-include 'src/View.php';
