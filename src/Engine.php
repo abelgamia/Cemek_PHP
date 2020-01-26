72c3475bf65300611e5dbd2078b840daa0546f36
Edikowy <edikowy@vp.pl>
2020-01-26 02:30:45 +0100

    engine cd2

--- src/Engine.php
+++ src/Engine.php
@@ -11,9 +11,12 @@ namespace src;
 class Engine {
     private $ses_name;
     private $ses_id;
-    private $kuki_name;
-    private $kuki_value;
-    private $kuki_exp = 3600*24*30;
+    public function startsSession() {
+        {
+            $this->startSession(null, null);
+        }
+        //         return session_id();
+    }
     public function startSession($ses_name, $ses_id) {
         {
             if (!empty($_SESSION)) {
@@ -25,6 +28,7 @@ class Engine {
         }
         return TRUE;
     }
+    
     public function __construct() {}
     public function start() {
         //----------------------------
@@ -50,9 +54,11 @@ class Engine {
     public function checkKuki($kuki_name) {
         return !isset($_COOKIE[$kuki_name]);
     }
+    public function checkKukis() {
+        return !isset($_COOKIE);
+    }
     public function getKuki($kuki_name) {
         if (isset($_COOKIE[$kuki_name])) {
-            // 	        $kuki_value = $_COOKIE[$kuki_name];
             return $_COOKIE[$kuki_name];
         } else {
             return FALSE;
@@ -62,84 +68,17 @@ class Engine {
         setcookie($kuki_name, $kuki_value, time()+$kuki_exp);
         return TRUE;
     }
-    /**
-     * @return mixed
-     */
-    public function getSes_name()
-    {
+    public function getSes_name() {
         return $this->ses_name;
     }
-    
-    /**
-     * @return mixed
-     */
-    public function getSes_id()
-    {
-        return $this->ses_id;
-    }
-    
-    /**
-     * @return mixed
-     */
-    public function getKuki_name()
-    {
-        return $this->kuki_name;
-    }
-    
-    /**
-     * @return mixed
-     */
-    public function getKuki_value()
-    {
-        return $this->kuki_value;
-    }
-    
-    /**
-     * @return number
-     */
-    public function getKuki_exp()
-    {
-        return $this->kuki_exp;
-    }
-    
-    /**
-     * @param mixed $ses_name
-     */
-    public function setSes_name($ses_name)
-    {
+    public function setSes_name($ses_name) {
         $this->ses_name = $ses_name;
     }
-    
-    /**
-     * @param mixed $ses_id
-     */
-    public function setSes_id($ses_id)
-    {
+    public function getSes_id() {
+        return $this->ses_id;
+    }
+    public function setSes_id($ses_id) {
         $this->ses_id = $ses_id;
-    }
-    
-    /**
-     * @param mixed $kuki_name
-     */
-    public function setKuki_name($kuki_name)
-    {
-        $this->kuki_name = $kuki_name;
-    }
-    
-    /**
-     * @param mixed $kuki_value
-     */
-    public function setKuki_value($kuki_value)
-    {
-        $this->kuki_value = $kuki_value;
-    }
-    
-    /**
-     * @param number $kuki_exp
-     */
-    public function setKuki_exp($kuki_exp)
-    {
-        $this->kuki_exp = $kuki_exp;
     }
     
 }
